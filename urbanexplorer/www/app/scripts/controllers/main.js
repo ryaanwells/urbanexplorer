ea4cb15ea0182a40fb120e51833cc17bf4f5d145
Ryan Wells <ryaanwells@googlemail.com>
2013-10-13 23:11:26 +0100

    Split up into seperate files

--- urbanexplorer/www/app/scripts/controllers/main.js
+++ urbanexplorer/www/app/scripts/controllers/main.js
@@ -1,43 +1,8 @@
 'use strict';
 
-UrbanExplorer.factory('geolocation', function ($rootScope) {
-  return {
-    getCurrentPosition: function (onSuccess, onError, options) {
-      navigator.geolocation.getCurrentPosition(
-	function () {
-          var that = this,
-          args = arguments;
-	  console.log("Success callback");
-          if (onSuccess) {
-	    console.log("success");
-            $rootScope.$apply(function () {
-              onSuccess.apply(that, args);
-            });
-          }
-	}, function () {
-          var that = this,
-          args = arguments;
-	  console.log("Failure callback");
-          if (onError) {
-	    console.log("fail");
-            $rootScope.$apply(function () {
-              onError.apply(that, args);
-            });
-          }
-	},
-	{enableHighAccuracy : true});
-    }
-  };
-});
-UrbanExplorer.directive('maximise', function(){
-  return function($scope, $elem, $attrs){
-    $elem.css('height', window.innerHeight + 'px');
-    $elem.css('width',  window.innerWidth  + 'px');
-  };
-});
 UrbanExplorer.controller('MainCtrl', function($scope, geolocation, $location) {
-
-  geolocation.getCurrentPosition(function (position) {
+  /*
+  geolocation.getCurrentPosition().then(function (position) {
     console.log(position);
     alert('Latitude: '              + position.coords.latitude          + '\n' +
           'Longitude: '             + position.coords.longitude         + '\n' +
@@ -51,12 +16,16 @@ UrbanExplorer.controller('MainCtrl', function($scope, geolocation, $location) {
     console.log(error.code);
     console.log(error.message);
   });
+  */
   
   $scope.swipeLeft = function(){
     $location.path("/targets/");
   }
   $scope.swipeRight = function(){
     $location.path("/achievements/");
+  }
+  $scope.coords = function(){
+    return geolocation.getCoordinatesList();
   }
 });
 
---
+++ urbanexplorer/www/app/scripts/core/location.js
@@ -0,0 +1,41 @@
+'use strict';
+
+UrbanExplorer.factory('geolocation', function ($rootScope, $q) {
+  var coordinates = [];
+  
+  var getCurrentPosition = function () {
+    var deferred = $q.defer();
+    navigator.geolocation.getCurrentPosition(
+      function (location) {
+        $rootScope.$apply(function () {
+	  deferred.resolve(location);
+        });
+      }, function (error) {
+        $rootScope.$apply(function () {
+          deferred.reject(location);
+        });
+      },
+      {enableHighAccuracy : true});
+    return deferred.promise;
+  }
+  
+  var pollPosition = function(){
+    getCurrentPosition.then(
+      function(coords){
+	coordinates.push([coords.coords.latitude, coords.coords.longitude]);
+	pollPosition();
+      },
+      function(error){
+	alert(error);
+      });
+  }
+
+  var getCoordinatesList = function(){
+    return coordinates;
+  }
+
+  return {
+    pollPosition: pollPosition,
+    getCoordinatesList: getCoordinatesList
+  };
+});
